b3f76f22c5281dc661089c73780654dd3e3f6c79
Riescent <91223304+Riescent@users.noreply.github.com>
2022-12-03 18:07:25 +0100

    Made infinite rotation possible

--- srcs/move_camera.c
+++ srcs/move_camera.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 18:12:39 by vfries            #+#    #+#             */
-/*   Updated: 2022/12/02 01:15:35 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2022/12/03 18:04:42 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,14 @@ void	move_camera_y_x(t_fdf *fdf, int *keys)
 	}
 }
 
+static void	secure_rot(double *rot)
+{
+	if (*rot > M_PI * 4)
+		*rot -= M_PI * 4;
+	else if (*rot < M_PI * 4)
+		*rot += M_PI * 4;
+}
+
 void	move_camera_up_down_rotation_z(t_fdf *fdf, int *keys)
 {
 	if (keys[KEY_SPACE])
@@ -60,11 +68,13 @@ void	move_camera_up_down_rotation_z(t_fdf *fdf, int *keys)
 	if (keys[KEY_E])
 	{
 		fdf->mats.rot_z.rot += CAM_SPEED / M_PI;
+		secure_rot(&fdf->mats.rot_z.rot);
 		update_rot_z_matrix(&fdf->mats);
 	}
 	if (keys[KEY_Q])
 	{
 		fdf->mats.rot_z.rot -= CAM_SPEED / M_PI;
+		secure_rot(&fdf->mats.rot_z.rot);
 		update_rot_z_matrix(&fdf->mats);
 	}
 }
@@ -74,21 +84,25 @@ void	move_camera_rotation(t_fdf *fdf, int *keys)
 	if (keys[KEY_UP])
 	{
 		fdf->mats.rot_x.rot += CAM_SPEED / M_PI;
+		secure_rot(&fdf->mats.rot_x.rot);
 		update_rot_x_matrix(&fdf->mats);
 	}
 	if (keys[KEY_DOWN])
 	{
 		fdf->mats.rot_x.rot -= CAM_SPEED / M_PI;
+		secure_rot(&fdf->mats.rot_x.rot);
 		update_rot_x_matrix(&fdf->mats);
 	}
 	if (keys[KEY_LEFT])
 	{
 		fdf->mats.rot_y.rot += CAM_SPEED / M_PI;
+		secure_rot(&fdf->mats.rot_y.rot);
 		update_rot_y_matrix(&fdf->mats);
 	}
 	if (keys[KEY_RIGHT])
 	{
 		fdf->mats.rot_y.rot -= CAM_SPEED / M_PI;
+		secure_rot(&fdf->mats.rot_y.rot);
 		update_rot_y_matrix(&fdf->mats);
 	}
 }
